5cca0009679bb49a291418019f62a2ff4a8694fc
nic-instruction <nicole.bade@seattlecolleges.edu>
2018-05-24 19:07:34 -0700

    Update nti-320-check.sh

--- nti-320-check.sh
+++ nti-320-check.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
 # This is a script meant to let us test NRPE
 # Plugins that we write
+cycle=$( uptime | awk '{ print $10 }' | awk -F "," '{print $1}')                                # Change the status to test different alert states
 
-status="0"                                  # Change the status to test different alert states
-
-if [ $status == "0" ]; then
+if [ $cycle =<"0.50" ]; then
     echo "STATUS:OK"
     exit 0;
     
-  elif [ $status == "2" ]; then
+  elif [ $status >= "0.70" ]; then
     echo "STATUS:CRITICAL"
     exit 2;
     
-  elif [ $status == "1" ] ; then
+  elif [ $status >= "0.51" ] ; then
     echo "STATUS:WARNING"
     exit 1;
     
